ea9279ccdfba321bff89b286d4574af5f10c688f
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-02-26 04:34:13 -0300

    added functions to validate form in page createPost

--- src/pages/Create-post/index.js
+++ src/pages/Create-post/index.js
@@ -1,12 +1,42 @@
+import { useState } from 'react';
+
 import FormPost from '../../components/Form-post';
 
 export default function CreatePost() {
+  const [formValues, setFormValues] = useState({ title: '', content: '' });
+  const [formErrors, setFormErrors] = useState({ title: '', content: '' });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setFormValues((lastValues) => ({ ...lastValues, [name]: value }));
+    setFormErrors((lastValues) => ({ ...lastValues, [name]: '' }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { title, content } = formValues;
+
+    if (!title) {
+      setFormErrors((lastErrors) => ({ ...lastErrors, title: 'Title is required' }));
+    }
+
+    if (!content) {
+      setFormErrors((lastErrors) => ({ ...lastErrors, content: 'Content is required' }));
+    }
+
+    if (title && content) {
+      console.log(formValues);
+    }
+  };
+
   return (
-    <FormPost
-      data={{ title: 'pepe', content: 'popo' }}
-      errors={{ title: 'jj', content: '' }}
-      onChange={(e) => console.log(e.target.value)}
-      onSubmit={() => console.log('pepe')}
-    />
+    <>
+      <h2 className="mb-4 font-weight-light">Create a new post</h2>
+      <FormPost
+        data={formValues}
+        errors={formErrors}
+        onChange={handleChange}
+        onSubmit={onSubmit}
+      />
+    </>
   );
 }

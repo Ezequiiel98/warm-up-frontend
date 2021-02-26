0b95943ffdffa3df7a3b63ef2510a433d98762fd
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-02-26 10:23:28 -0300

    added functions to get and show posts

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,36 @@
+import { useEffect, useState } from 'react';
+
 import CardPost from '../../components/Card-post';
 
+import { getAllPost } from '../../services/post.service';
+
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const res = await getAllPost();
+        setPosts(res.data);
+      } catch {
+        setError(true);
+      }
+    };
+    getPosts();
+    return null;
+  }, []);
+
   const handleDelete = id => {
     console.log(id);
   };
 
   return (
     <div className="d-flex justify-content-between flex-wrap">
-      <CardPost id="5" title="juanchooote" onDelete={handleDelete} />
+      { error && <h2 className="text-center w-100 mt-3">Something is wrong 😢, try later</h2>}
+      { !error && posts.map(({ title, id }) => (
+        <CardPost id={id} title={title} onDelete={handleDelete} />
+      ))}
     </div>
   );
 }

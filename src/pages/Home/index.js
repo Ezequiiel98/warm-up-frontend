5b26e5dd9a602758d7964eaf369ea07eecac2bc6
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-02-26 10:44:23 -0300

    added function to delete post and modal to show messages

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 
 import CardPost from '../../components/Card-post';
+import Modal from '../../components/Modal';
 
-import { getAllPost } from '../../services/post.service';
+import { getAllPost, deletePost } from '../../services/post.service';
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(false);
+  const [modalData, setModalData] = useState({ show: false, message: '' });
 
   useEffect(() => {
     const getPosts = async () => {
@@ -21,16 +23,41 @@ export default function Home() {
     return null;
   }, []);
 
-  const handleDelete = id => {
-    console.log(id);
+  const handleDelete = async id => {
+    try {
+      await deletePost(id);
+      const filteredPosts = posts.filter(post => post.id !== id);
+      setModalData({
+        title: 'Post deleted',
+        message: 'Post deleted successfully.',
+        show: true,
+      });
+      setPosts(filteredPosts);
+    } catch {
+      setModalData({
+        title: 'Error',
+        message: 'Something is wrong, the post could not be deleted. Please try later.',
+        show: true,
+      });
+    }
+  };
+
+  const handleCloseModal = () => {
+    setModalData({ show: false, message: '', title: '' });
   };
 
   return (
     <div className="d-flex justify-content-between flex-wrap">
       { error && <h2 className="text-center w-100 mt-3">Something is wrong 😢, try later</h2>}
       { !error && posts.map(({ title, id }) => (
-        <CardPost id={id} title={title} onDelete={handleDelete} />
+        <CardPost id={id} title={title} onDelete={handleDelete} key={id} />
       ))}
+      <Modal
+        title={modalData.title}
+        message={modalData.message}
+        show={modalData.show}
+        onClose={handleCloseModal}
+      />
     </div>
   );
 }
